23f79af2e2d23a63fdb53e01ebadc45ad74747a7
Justus W <65566771+FreakyLetsFail@users.noreply.github.com>
2024-10-01 21:07:04 +0200

    changed node_helper

--- node_helper.js
+++ node_helper.js
@@ -1,4 +1,5 @@
 var NodeHelper = require("node_helper");
+const ical = require("ical");
 const axios = require("axios");
 
 module.exports = NodeHelper.create({
@@ -14,19 +15,38 @@ module.exports = NodeHelper.create({
 
   getCalendarData: function (url) {
     var self = this;
+
+    // Lade die Daten von der Kalender-URL herunter
     axios.get(url)
       .then((response) => {
-        const calendarData = self.parseCalendarData(response.data);
-        self.sendSocketNotification("CALENDAR_DATA_RECEIVED", calendarData);
+        const calendarData = ical.parseICS(response.data); // Parse die ICS-Daten
+        const events = self.formatCalendarData(calendarData);
+        self.sendSocketNotification("CALENDAR_DATA_RECEIVED", events);
       })
       .catch((error) => {
         console.error("Error fetching calendar data: ", error);
       });
   },
 
-  parseCalendarData: function (icsData) {
-    // Hier kannst du die ics-Datei parsen
-    const parsedData = []; // Kalender-Events hier hinein parsen
-    return parsedData;
+  formatCalendarData: function (calendarData) {
+    const events = [];
+
+    for (const k in calendarData) {
+      const event = calendarData[k];
+      if (event.type === "VEVENT") {
+        events.push({
+          title: event.summary,
+          startTime: this.formatTime(event.start),
+          endTime: this.formatTime(event.end)
+        });
+      }
+    }
+
+    return events;
+  },
+
+  formatTime: function (dateTime) {
+    const date = new Date(dateTime);
+    return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
   }
 });
