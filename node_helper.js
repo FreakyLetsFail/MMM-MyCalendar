1af7f2d21b0cebfb2e7955815328b28bc11ab211
Justus W <65566771+FreakyLetsFail@users.noreply.github.com>
2024-10-13 18:33:51 +0200

    changes on node-helper

--- node_helper.js
+++ node_helper.js
@@ -9,51 +9,51 @@ module.exports = NodeHelper.create({
 
   socketNotificationReceived: function (notification, payload) {
     if (notification === "GET_CALENDAR_DATA") {
-      this.getCalendarData(payload.urls, payload.eventSettings); // Event-Einstellungen übergeben
+      this.getCalendarData(payload.urls, payload.eventSettings);
     }
   },
 
   getCalendarData: async function (urls, eventSettings) {
     const self = this;
     const events = [];
-  
+
     const fetchPromises = Object.entries(urls).map(([url, category]) => {
       return new Promise((resolve, reject) => {
         const httpsUrl = url.replace("webcal://", "https://");
-  
+
         https.get(httpsUrl, (res) => {
           let data = "";
-  
+
           res.on("data", (chunk) => {
             data += chunk;
           });
-  
+
           res.on("end", () => {
             const calendarData = ical.parseICS(data);
-  
+
             for (const key in calendarData) {
               const event = calendarData[key];
               if (event.type === "VEVENT") {
                 // Loggen der rohen Datumswerte
                 console.log("Raw event.start:", event.start);
                 console.log("Raw event.end:", event.end);
-  
-                // Angepasstes Parsing der Datumswerte
-                const startTime = parseCustomDateTime(event.start);
-                const endTime = parseCustomDateTime(event.end);
-  
+
+                // Direktes Parsen der Datumswerte
+                const startTime = new Date(event.start);
+                const endTime = new Date(event.end);
+
                 // Überprüfen auf gültige Datumswerte
                 if (isNaN(startTime) || isNaN(endTime)) {
                   console.error("Ungültiges Datum für Ereignis:", event.summary);
                   continue; // Überspringe dieses Ereignis
                 }
-  
+
                 let description = event.description || "";
                 description += `\nCategory: ${category}`;
-  
+
                 const icon = eventSettings[category]?.icon || "fas fa-calendar-alt";
                 const color = eventSettings[category]?.color || "#FFFFFF";
-  
+
                 events.push({
                   title: event.summary,
                   startTime: startTime,
@@ -72,7 +72,7 @@ module.exports = NodeHelper.create({
         });
       });
     });
-  
+
     try {
       await Promise.all(fetchPromises);
       console.log("Gesamtzahl der verarbeiteten Ereignisse:", events.length);
